05cf68751ccaa932894b18064ec4b91dd8688b5e
Nikolay <kolyak770@gmail.com>
2023-01-16 12:39:19 +0300

    feat: create necessary common types and constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,45 @@
+export const MESSAGES: CodeMessages = {
+  SUCCESS: {
+    code: 200,
+    message: '',
+  },
+  CREATED: {
+    code: 201,
+    message: '',
+  },
+  USER_DELETED: {
+    code: 204,
+    message: '',
+  },
+  INVALID_ID: {
+    code: 400,
+    message: 'Invalid uuid.',
+  },
+  INVALID_DATA: {
+    code: 400,
+    message: 'Invalid data.',
+  },
+  USER_NOT_FOUND: {
+    code: 404,
+    message: "User doesn't exist.",
+  },
+  INVALID_RESOURCE: {
+    code: 404,
+    message: 'No such a resource.',
+  },
+  SERVER_ERROR: {
+    code: 500,
+    message: 'Internal server error.',
+  },
+};
+
+export const REQUIRED_KEYS = ['username', 'age', 'hobbies'];
+
+type CodeMessages = {
+  [key: string]: CodeType;
+};
+
+type CodeType = {
+  code: number;
+  message: string;
+};
